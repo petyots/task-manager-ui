1eb8a428351cde8bf7fdd745ecd9dce1f3403ed8
Petyo Tsonev <petyo@sterxa.com>
2021-11-05 15:48:23 +0200

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <p>Our Url is: {{ $config.apiBaseURL}}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+console.log()
+export default Vue.extend({})
+</script>
